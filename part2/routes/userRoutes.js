9498ed48b4d7dc25785c079c39c2412b2cbed8f2
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 08:43:19 +0000

    [automatic commit] 2025-06-20 08:43:19 UTC 129.127.202.107

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
   try {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
-      WHERE user = ? AND password_hash = ?
+      WHERE user_id = ? AND password_hash = ?
     `, [user, password]);
 
     if (rows.length === 0) {
