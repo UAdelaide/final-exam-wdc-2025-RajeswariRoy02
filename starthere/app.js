9b83d74ae6dafd59df56232a44414eaa7a95e296
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 04:06:01 +0000

    [automatic commit] 2025-06-20 04:06:01 UTC 129.127.202.107

--- starthere/app.js
+++ starthere/app.js
@@ -116,9 +116,9 @@ let db;
 })();
 
 // Route to return books as JSON
-app.get('/', async (req, res) => {
+app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
+    const [dogs] = await db.execute('SELECT * FROM books');
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
