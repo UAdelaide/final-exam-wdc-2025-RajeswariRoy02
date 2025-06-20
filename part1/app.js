16cde8298a356d8faa62bb0e90fbc8a361e2e434
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 04:37:39 +0000

    [automatic commit] 2025-06-20 04:37:39 UTC 129.127.202.107

--- part1/app.js
+++ part1/app.js
@@ -4,9 +4,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mysql = require('mysql2/promise');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiModule = require('./routes/api');
 
 var app = express();
 
