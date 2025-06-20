bdb0017f762f1ea1530c23f99297a6ae9011cb7b
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 12:06:13 +0000

    [automatic commit] 2025-06-20 12:06:13 UTC 129.127.202.107

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -70,7 +70,7 @@ router.post('/logout',(req,res)=>{
     req.session.destroy(err => {
       if (err) {
         console.error("Error destroying session:", err);
-        return res.status(500).json({ message: 'Could not log out, please try again
+        return res.status(500).json({ message: 'Could not log out, please try again.' });
 
 })
 
