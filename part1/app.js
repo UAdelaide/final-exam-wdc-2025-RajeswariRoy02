4555910c7d239fb73eb55c24f992007f5f56c18d
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 04:39:02 +0000

    [automatic commit] 2025-06-20 04:39:02 UTC 129.127.202.107

--- part1/app.js
+++ part1/app.js
@@ -165,7 +165,7 @@ app.get('/api/walkers/summary', async (req, res) => {
     `);
     res.json(summary);
       }catch (err){
-        res.status(500).json({error:'Failed to fetch walker summary'});
+        res.status(500).json({ error: 'Failed to fetch walker summary'});
 
       }
     });
