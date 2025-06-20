45bc7cfd35fbb5373ff5729cb6cf495c494e3c73
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 04:36:59 +0000

    [automatic commit] 2025-06-20 04:36:59 UTC 129.127.202.107

--- part1/app.js
+++ part1/app.js
@@ -16,10 +16,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+let db;
+
 
 (async () => {
+    try{}
     const connection = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
