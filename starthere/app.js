5f4be8707ae2063463ba7810c9917abb2dd3fe67
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 04:02:15 +0000

    [automatic commit] 2025-06-20 04:02:15 UTC 129.127.202.107

--- starthere/app.js
+++ starthere/app.js
@@ -83,7 +83,7 @@ let db;
 
     // Insert data if table is empty
     const [userCount] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-    if (rows[0].count === 0) {
+    if (userCount[0].count === 0) {
       await db.execute(`
         INSERT INTO books (title, author) VALUES
         ('1984', 'George Orwell'),
