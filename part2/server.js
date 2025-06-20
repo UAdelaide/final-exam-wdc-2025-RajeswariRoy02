877a340c6813e385e58376f5dccabf30059e2e1d
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 09:16:32 +0000

    [automatic commit] 2025-06-20 09:16:32 UTC 129.127.202.107

--- part2/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app'); // Import your Express app
-const port = process.env.PORT || 3000; // Use port from environment variable or default to 3000
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
