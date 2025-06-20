478c0c2899be6ce6856f9f42a7b66892f19a1786
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 04:27:40 +0000

    [automatic commit] 2025-06-20 04:27:40 UTC 129.127.202.107

--- part1/app.js
+++ part1/app.js
@@ -91,10 +91,10 @@ if (userCount.count === 0) {
     `);
     await db.execute(`
         INSERT INTO Dogs (owner_id, name, size) VALUES
-((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-((SELECT user_id FROM Users WHERE username = 'alice123'), 'Vulu', 'large'),
-((SELECT user_id FROM Users WHERE username = 'carol123'), 'Tommy', 'medium'),
-((SELECT user_id FROM Users WHERE username = 'edward34'), 'Butter', 'small');
+        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Vulu', 'large'),
+        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Tommy', 'medium'),
+        ((SELECT user_id FROM Users WHERE username = 'edward34'), 'Butter', 'small');
         `)
 module.exports = app;
