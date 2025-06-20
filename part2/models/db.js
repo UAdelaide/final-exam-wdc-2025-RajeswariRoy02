3dc4516d9b15116f11513f93864ef73f324c86fa
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 13:40:54 +0000

    [automatic commit] 2025-06-20 13:40:54 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.107

--- part2/models/db.js
+++ part2/models/db.js
@@ -3,7 +3,6 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password:'1234',
   database: 'DogWalkService',
   waitForConnections: true,
   connectionLimit: 10,
