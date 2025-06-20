aee4d1e65db4c193fe5f64c82336024ea32af196
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 12:29:01 +0000

    [automatic commit] 2025-06-20 12:29:00 UTC 129.127.202.107

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -59,14 +59,12 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
 ('roy12', 'roy@example.com', 'hashed987', 'walker'),
 ('edward34', 'cullen@example.com', 'hashed321', 'owner'); 
-
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Vulu', 'large'),
 ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Tommy', 'medium'),
 ((SELECT user_id FROM Users WHERE username = 'edward34'), 'Butter', 'small');
-
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
 ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
