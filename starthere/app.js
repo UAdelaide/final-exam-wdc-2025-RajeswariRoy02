afdc921c5b4eb28641beab2c79ef41d31802b994
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 04:11:54 +0000

    [automatic commit] 2025-06-20 04:11:54 UTC 129.127.202.107

--- starthere/app.js
+++ starthere/app.js
@@ -131,11 +131,7 @@ app.get('/api/dogs', async (req, res) => {
 //api for walkrequest
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [walkRequests] = await db.execute(`SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
-    FROM WalkRequests wr
-    JOIN Dogs d ON wr.dog_id = d.dog_id
-    JOIN Users u ON d.owner_id = u.user_id
-    WHERE wr.status = 'open'
+    const [walkRequests] = await db.execute(`
     `);
     res.json(walkRequests);
   } catch (err) {
