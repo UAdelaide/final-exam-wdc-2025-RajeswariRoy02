bab99697d5d34b87822e3905a2d8c0821ea8e3d1
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 12:03:04 +0000

    [automatic commit] 2025-06-20 12:03:04 UTC 129.127.202.107

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -65,6 +65,8 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Login failed' });
   }
 });
-router.post('/logout',(req,res))
+router.post('/logout',(req,res)=>{
+  
+})
 
 module.exports = router;
