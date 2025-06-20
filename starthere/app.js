f5b0b844c7e44562c159fe3001c90c86211ed3c2
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 03:57:30 +0000

    [automatic commit] 2025-06-20 03:57:29 UTC 129.127.202.107

--- starthere/app.js
+++ starthere/app.js
@@ -31,7 +31,7 @@ let db;
       host: 'localhost',
       user: 'root',
       password: '',
-      database: 'testdb'
+      database: 'DoggyWalky'
     });
 
     // Create a table if it doesn't exist
