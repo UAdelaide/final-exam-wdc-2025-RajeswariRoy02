407cc56a7b89209a946dafa99deb83c7dad77303
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 08:25:22 +0000

    [automatic commit] 2025-06-20 08:25:22 UTC 129.127.202.107

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,8 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
     req.session.user ={
-      id:rows
+      id:rows[0].user_id,
+      
     }
 
 
