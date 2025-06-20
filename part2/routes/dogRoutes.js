af22332d6139434ba7c519b77b91b6998b76fe4a
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 13:08:17 +0000

    [automatic commit] 2025-06-20 13:08:17 UTC 129.127.202.107

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 const db= require('../models/db');
 
+function isAuthenticated(req, res, next) {
+    if (req.session && req.session.user) {
+        return next();
+    }
+    res.status(401).json({ error: 'Unauthorized: Please log in.' });
+}
