db2bcfc03df2535f8a83a6ae46942d3bc092e446
RajeswariRoy1 <proximarroy@gmail.com>
2025-06-20 04:20:30 +0000

    [automatic commit] 2025-06-20 04:20:30 UTC 129.127.202.107

--- part1/app.js
+++ part1/app.js
@@ -19,5 +19,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+(async () => {
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+    });
 
 module.exports = app;
